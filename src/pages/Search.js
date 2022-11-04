8efd84478b3934d8c2ee7045895f99bc3201b82a
Daniel Oliveira <danoliveira865@gmail.com>
2022-11-04 18:48:17 -0300

    Adiciona formulario de pesquisa no Search.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,12 +2,55 @@ import React, { Component } from 'react';
 import Header from '../Components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: '',
+      isBtnDisabled: true,
+      isLoading: false,
+    };
+  }
+
+  hendleChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value,
+    }, () => this.validateForm());
+  };
+
+  validateForm = () => {
+    const { searchInput } = this.state;
+    const searchInputSize = searchInput.length > 1;
+    this.setState({
+      isBtnDisabled: !searchInputSize,
+    });
+  };
+
   render() {
+    const { searchInput, isBtnDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <label htmlFor="searchInput">
+            <input
+              type="text"
+              id="searchInput"
+              data-testid="search-artist-input"
+              name="searchInput"
+              value={ searchInput }
+              onChange={ this.hendleChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isBtnDisabled }
+          >
+            Pesquisar
 
+          </button>
+        </form>
       </div>
     );
   }
