ff03c0ec2ed3e66cf9015833320bbde78339c4e2
Daniel Oliveira <danoliveira865@gmail.com>
2022-11-04 16:38:01 -0300

    Adiciona componente Loading e ativação do botão de login. Requisito 2 finalizado.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,85 @@
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      isBtnDisabled: true,
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCreateUserAPI();
+  }
+
+  fetchCreateUserAPI = async () => {
+    const { userName } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+    await createUser({ name: userName });
+    this.setState({
+      isLoading: false,
+    });
+    if (userName) {
+      this.pushHistory('/search');
+    }
+  };
+
+  pushHistory = (pathName) => {
+    const { history } = this.props;
+    history.push(pathName);
+  };
+
+  hendleChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value,
+    }, () => this.validateForm());
+  };
+
+  validateForm = () => {
+    const { userName } = this.state;
+    const userNameSize = userName.length > 2;
+    this.setState({
+      isBtnDisabled: !userNameSize,
+    });
+  };
+
   render() {
+    const { userName, isBtnDisabled, isLoading } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        {isLoading ? <Loading /> : (
+          <form>
+            <label htmlFor="userName">
+              Usuário:
+              <input
+                type="text"
+                id="userName"
+                data-testid="login-name-input"
+                name="userName"
+                value={ userName }
+                onChange={ this.hendleChange }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ isBtnDisabled }
+              onClick={ this.fetchCreateUserAPI }
+            >
+              Entrar
+
+            </button>
+          </form>
+        ) }
+
+      </div>
     );
   }
 }
